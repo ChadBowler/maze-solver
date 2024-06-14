37208c10b9a536d82a2f66255e35563eaaccb2b2
ChadBowler <cbowlerdev@gmail.com>
2024-06-14 13:51:04 -0600

    added point and line classes to draw lines

--- graphics.py
+++ graphics.py
@@ -6,22 +6,42 @@ class Window():
         self.__root.title("Maze Solver")
         self.__root.geometry(f"{width}x{height}")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        # Initially set to fullscreen
         self.__root.attributes("-fullscreen", True)
+        # Binding keys to exit and enter fullscreen mode
         self.__root.bind("<F11>", lambda event: self.__root.attributes("-fullscreen",
                                     not self.__root.attributes("-fullscreen")))
         self.__root.bind("<Escape>", lambda event: self.__root.attributes("-fullscreen", False))
-        self.canvas = Canvas(self.__root, bg="red", height="800", width="800")
-        self.canvas.pack()
-        self.running = False
+        # Set up canvas
+        self.__canvas = Canvas(self.__root, bg="red", height="800", width="800")
+        self.__canvas.pack()
+        self.__running = False
         
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
 
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self.__running = True
+        while self.__running:
             self.redraw()
     
     def close(self):
-        self.running = False
+        self.__running = False
+    
+    def draw_line(self, line, fill_color):
+        line = line.draw(self.__canvas, fill_color)
+
+class Point():
+    def __init__(self, x, y):
+        self.x=x
+        self.y=y
+
+class Line():
+    def __init__(self, point_a, point_b):
+        self.point_a = point_a
+        self.point_b = point_b
+
+    def draw(self, canvas, fill_color="black"):
+        self.fill_color = fill_color
+        canvas.create_line(self.point_a.x, self.point_a.y, self.point_b.x, self.point_b.y, fill=fill_color, width=2)
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
+    # Create new window with 1200 X 1200 dimensions
     win = Window(1200, 1200)
+    # Example line draw
+    point_a = Point(50, 10)
+    point_b = Point(510, 110)
+    line = Line(point_a, point_b)
+    win.draw_line(line, "green")
+    # continuously call redraw while program is running
     win.wait_for_close()
 
 if __name__ == "__main__":
