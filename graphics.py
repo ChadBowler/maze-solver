d9871c87c91fa5af134d268a378daa6dde7e69b3
ChadBowler <cbowlerdev@gmail.com>
2024-06-17 14:25:17 -0600

    added Cell class with draw method

--- graphics.py
+++ graphics.py
@@ -1,5 +1,11 @@
 from tkinter import Tk, BOTH, Canvas
 
+RED="red"
+GREEN="green"
+BLACK="black"
+WHITE="white"
+line_width = 2
+
 class Window():
     def __init__(self, height=1200, width=1200):
         self.__root = Tk()
@@ -7,13 +13,13 @@ class Window():
         self.__root.geometry(f"{width}x{height}")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
         # Initially set to fullscreen
-        self.__root.attributes("-fullscreen", True)
+        self.__root.attributes("-zoomed", True)
         # Binding keys to exit and enter fullscreen mode
         self.__root.bind("<F11>", lambda event: self.__root.attributes("-fullscreen",
                                     not self.__root.attributes("-fullscreen")))
         self.__root.bind("<Escape>", lambda event: self.__root.attributes("-fullscreen", False))
         # Set up canvas
-        self.__canvas = Canvas(self.__root, bg="red", height="800", width="800")
+        self.__canvas = Canvas(self.__root, bg=WHITE, height="800", width="800")
         self.__canvas.pack()
         self.__running = False
         
@@ -42,6 +48,38 @@ class Line():
         self.point_a = point_a
         self.point_b = point_b
 
-    def draw(self, canvas, fill_color="black"):
+    def draw(self, canvas, fill_color=BLACK):
         self.fill_color = fill_color
-        canvas.create_line(self.point_a.x, self.point_a.y, self.point_b.x, self.point_b.y, fill=fill_color, width=2)
+        canvas.create_line(self.point_a.x, self.point_a.y, self.point_b.x, self.point_b.y, fill=fill_color, width=line_width)
+
+class Cell():
+    def __init__(self, x1, y1, x2, y2, win, has_left_wall=True, has_right_wall=True, has_top_wall=True, has_bottom_wall=True):
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        self._win = win
+        self.has_left_wall = has_left_wall
+        self.has_right_wall = has_right_wall
+        self.has_top_wall = has_top_wall
+        self.has_bottom_wall = has_bottom_wall
+
+    def draw_cell(self):
+        tl_corner = Point(self._x1, self._y1)
+        tr_corner = Point(self._x2, self._y1)
+        bl_corner = Point(self._x1, self._y2)
+        br_corner = Point(self._x2, self._y2)
+        
+        if self.has_left_wall:
+            left_wall = Line(tl_corner, bl_corner)
+            self._win.draw_line(left_wall, BLACK) 
+        if self.has_right_wall:
+            right_wall = Line(tr_corner, br_corner)
+            self._win.draw_line(right_wall, BLACK) 
+        if self.has_top_wall:
+            top_wall = Line(tl_corner, tr_corner)
+            self._win.draw_line(top_wall, BLACK) 
+        if self.has_bottom_wall:
+            bottom_wall = Line(bl_corner, br_corner)
+            self._win.draw_line(bottom_wall, BLACK)
+        
--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from graphics import Window, Point, Line
+from graphics import Window, Point, Line, Cell
 
 def main():
     # Create new window with 1200 X 1200 dimensions
     win = Window(1200, 1200)
+    # Example cell
+    cells = [
+        Cell(5, 5, 50, 50, win, True, True, True, True),
+        Cell(50, 5, 100, 50, win, True, False, True, True),
+        Cell(100, 5, 150, 50, win, True, True, False, True),
+        Cell(150, 5, 200, 50, win, True, True, True, False),
+        Cell(605, 755, 650, 800, win, True, False, True, True),
+        Cell(650, 755, 700, 800, win, False, False, False, False),
+        Cell(700, 755, 750, 800, win, False, True, False, True),
+        Cell(750, 755, 800, 800, win, True, True, True, False)
+    ]
+    for cell in cells:
+        cell.draw_cell()
+    
     # Example line draw
-    point_a = Point(50, 10)
-    point_b = Point(510, 110)
-    line = Line(point_a, point_b)
-    win.draw_line(line, "green")
+    # point_a = Point(50, 10)
+    # point_b = Point(510, 110)
+    # line = Line(point_a, point_b)
+    # win.draw_line(line, "black")
+
     # continuously call redraw while program is running
     win.wait_for_close()
 
