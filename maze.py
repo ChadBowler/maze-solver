c6a7736083ac2074ec83b153b0df053c55af0e31
ChadBowler <cbowlerdev@gmail.com>
2024-06-17 16:20:19 -0600

    filled out maze class and methods

--- maze.py
+++ maze.py
@@ -1,14 +1,38 @@
-
+from cells import Cell
+import time
 
 class Maze():
     def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
-        pass
+        self._cells = []
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
 
     def _create_cells(self):
-        pass
+        for i in range(self._num_cols):
+            col = []
+            for j in range(self._num_rows):
+                x1 = self._x1 + (i * self._cell_size_x)
+                y1 = self._y1 + (j * self._cell_size_y)
+                x2 = x1 + self._cell_size_x
+                y2 = y1 + self._cell_size_y
+                new_cell = Cell(x1, y1, x2, y2, self._win, True, True, True, True)
+                self._draw_cells(new_cell)
+                col.append(new_cell)
+            self._cells.append(col)
 
-    def _draw_cells(self, i, j):
-        pass
+    def _draw_cells(self, cell):
+        if self._win is None:
+            return
+        cell.draw_cell()
+        self._animate()
 
     def _animate(self):
-        pass
+        if self._win is None:
+            return
+        self._win.redraw()
+        time.sleep(.05)
