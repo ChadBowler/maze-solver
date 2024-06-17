90e9359a4433c8c165bd7035f43b4cd7f68b39a0
ChadBowler <cbowlerdev@gmail.com>
2024-06-17 15:11:40 -0600

    added maze class

---
+++ maze.py
@@ -0,0 +1,14 @@
+
+
+class Maze():
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        pass
+
+    def _create_cells(self):
+        pass
+
+    def _draw_cells(self, i, j):
+        pass
+
+    def _animate(self):
+        pass
